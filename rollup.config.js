71d2e97f7a5d12ab577166ce066be8c89be314f6
Takumi Stryder <nya.takumi.666@gmail.com>
2022-12-30 12:23:41 -0300

    Converted to CommonJS syntax and added settings to minified and unminified compilation

--- rollup.config.js
+++ rollup.config.js
@@ -1,51 +1,88 @@
-import { terser } from 'rollup-plugin-terser';
-import PeerDepsExternalPlugin from 'rollup-plugin-peer-deps-external';
-import { nodeResolve } from '@rollup/plugin-node-resolve';
-import commonjs from '@rollup/plugin-commonjs';
-import babel from '@rollup/plugin-babel';
-import postcss from 'rollup-plugin-postcss';
+const PeerDepsExternalPlugin = require('rollup-plugin-peer-deps-external');
+const { nodeResolve } = require('@rollup/plugin-node-resolve');
+const commonjs = require('@rollup/plugin-commonjs');
+const babel = require('@rollup/plugin-babel');
+const terser = require('@rollup/plugin-terser');
+const postcss = require('rollup-plugin-postcss');
 
-const packageJson = require('./package.json');
-
-export default {
-    input: './src/index.js',
-    output: [
-        {
-            file: packageJson.main,
-            format: 'cjs',
-            sourcemap: true
-        },
-        {
-            file: packageJson.module,
-            format: 'esm',
-            sourcemap: true
-        },
-        {
-            file: packageJson.browser,
-            format: 'umd',
-            name: 'ReactCalendator',
-            sourcemap: true
-        }
-    ],
-    plugins: [
-        PeerDepsExternalPlugin(),
-        nodeResolve({
-            extensions: [
-                '.js',
-                '.jsx'
-            ]
-        }),
-        commonjs(),
-        babel({
-            exclude: 'node_modules/**',
-            presets: [
-                '@babel/preset-react'
-            ],
-            babelHelpers: 'bundled'
-        }),
-        postcss({
-            minimize: true
-        }),
-        terser()
+const nodeResolveConfig = {
+    extensions: [
+        '.js',
+        '.jsx'
     ]
 };
+
+const babelConfig = {
+    exclude: 'node_modules/**',
+    presets: [
+        '@babel/preset-react'
+    ],
+    babelHelpers: 'bundled'
+};
+
+const postCSSConfig = {
+    minimize: true
+};
+
+module.exports = [
+    {
+        input: './src/index.js',
+        output: [
+            {
+                file: 'dist/cjs/react-calendator.js',
+                format: 'cjs',
+                sourcemap: true
+            },
+            {
+                file: 'dist/esm/react-calendator.js',
+                format: 'esm',
+                sourcemap: true
+            },
+            {
+                file: 'dist/umd/react-calendator.js',
+                format: 'umd',
+                name: 'ReactCalendator',
+                sourcemap: true
+            }
+        ],
+        plugins: [
+            PeerDepsExternalPlugin(),
+            nodeResolve(nodeResolveConfig),
+            commonjs(),
+            babel(babelConfig),
+            postcss(postCSSConfig)
+        ]
+    },
+    {
+        input: './src/index.js',
+        output: [
+            {
+                file: 'dist/cjs/react-calendator.min.js',
+                format: 'cjs',
+                sourcemap: true
+            },
+            {
+                file: 'dist/esm/react-calendator.min.js',
+                format: 'esm',
+                sourcemap: true
+            },
+            {
+                file: 'dist/umd/react-calendator.min.js',
+                format: 'umd',
+                name: 'ReactCalendator',
+                globals: {
+                    react: 'React'
+                },
+                sourcemap: true
+            }
+        ],
+        plugins: [
+            PeerDepsExternalPlugin(),
+            nodeResolve(nodeResolveConfig),
+            commonjs(),
+            babel(babelConfig),
+            postcss(postCSSConfig),
+            terser()
+        ]
+    }
+];
