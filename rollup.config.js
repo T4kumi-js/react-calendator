9e6638918822d1047975c18ac372f6c1c97ef095
Takumi Stryder <nya.takumi.666@gmail.com>
2023-02-25 22:24:39 -0300

    Changing indentation to two spaces

--- rollup.config.js
+++ rollup.config.js
@@ -6,83 +6,83 @@ const terser = require('@rollup/plugin-terser');
 const postcss = require('rollup-plugin-postcss');
 
 const nodeResolveConfig = {
-    extensions: [
-        '.js',
-        '.jsx'
-    ]
+  extensions: [
+    '.js',
+    '.jsx'
+  ]
 };
 
 const babelConfig = {
-    exclude: 'node_modules/**',
-    presets: [
-        '@babel/preset-react'
-    ],
-    babelHelpers: 'bundled'
+  exclude: 'node_modules/**',
+  presets: [
+    '@babel/preset-react'
+  ],
+  babelHelpers: 'bundled'
 };
 
 const postCSSConfig = {
-    minimize: true
+  minimize: true
 };
 
 module.exports = [
-    {
-        input: './src/index.js',
-        output: [
-            {
-                file: 'dist/cjs/react-calendator.js',
-                format: 'cjs',
-                sourcemap: true
-            },
-            {
-                file: 'dist/esm/react-calendator.js',
-                format: 'esm',
-                sourcemap: true
-            },
-            {
-                file: 'dist/umd/react-calendator.js',
-                format: 'umd',
-                name: 'ReactCalendator',
-                sourcemap: true
-            }
-        ],
-        plugins: [
-            PeerDepsExternalPlugin(),
-            nodeResolve(nodeResolveConfig),
-            commonjs(),
-            babel(babelConfig),
-            postcss(postCSSConfig)
-        ]
-    },
-    {
-        input: './src/index.js',
-        output: [
-            {
-                file: 'dist/cjs/react-calendator.min.js',
-                format: 'cjs',
-                sourcemap: true
-            },
-            {
-                file: 'dist/esm/react-calendator.min.js',
-                format: 'esm',
-                sourcemap: true
-            },
-            {
-                file: 'dist/umd/react-calendator.min.js',
-                format: 'umd',
-                name: 'ReactCalendator',
-                globals: {
-                    react: 'React'
-                },
-                sourcemap: true
-            }
-        ],
-        plugins: [
-            PeerDepsExternalPlugin(),
-            nodeResolve(nodeResolveConfig),
-            commonjs(),
-            babel(babelConfig),
-            postcss(postCSSConfig),
-            terser()
-        ]
-    }
+  {
+    input: './src/index.js',
+    output: [
+      {
+        file: 'dist/cjs/react-calendator.js',
+        format: 'cjs',
+        sourcemap: true
+      },
+      {
+        file: 'dist/esm/react-calendator.js',
+        format: 'esm',
+        sourcemap: true
+      },
+      {
+        file: 'dist/umd/react-calendator.js',
+        format: 'umd',
+        name: 'ReactCalendator',
+        sourcemap: true
+      }
+    ],
+    plugins: [
+      PeerDepsExternalPlugin(),
+      nodeResolve(nodeResolveConfig),
+      commonjs(),
+      babel(babelConfig),
+      postcss(postCSSConfig)
+    ]
+  },
+  {
+    input: './src/index.js',
+    output: [
+      {
+        file: 'dist/cjs/react-calendator.min.js',
+        format: 'cjs',
+        sourcemap: true
+      },
+      {
+        file: 'dist/esm/react-calendator.min.js',
+        format: 'esm',
+        sourcemap: true
+      },
+      {
+        file: 'dist/umd/react-calendator.min.js',
+        format: 'umd',
+        name: 'ReactCalendator',
+        globals: {
+          react: 'React'
+        },
+        sourcemap: true
+      }
+    ],
+    plugins: [
+      PeerDepsExternalPlugin(),
+      nodeResolve(nodeResolveConfig),
+      commonjs(),
+      babel(babelConfig),
+      postcss(postCSSConfig),
+      terser()
+    ]
+  }
 ];
